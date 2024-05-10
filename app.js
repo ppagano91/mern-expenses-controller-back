150735f47bae9f07c45ba1de095ddd3314f89428
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-09 23:45:44 -0300

    Corrección del Middleware de Errores + CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const userRouter = require('./routes/userRoutes')
+const mongoose = require('mongoose');
+const cors = require('cors')
+const userRouter = require('./routes/userRoutes');
 const categoryRouter = require('./routes/categoryRoutes');
 const transactionRouter = require('./routes/transactionRoutes');
 const errorHandler = require('./middlewares/errorHandlerMiddleware');
@@ -12,11 +13,22 @@ mongoose
     .connect('mongodb://localhost:27017/mern-expenses', {family:4})
     .then(() => console.log("DB Connected")).catch((e) => console.log(e))
 
+// CORS Config
+const corsOptions = {
+    origin: ['http://localhost:5173']
+}
+
+app.use(cors(corsOptions))
+
+// Middlewares
 app.use(express.json())
 
+// Routes
 app.use('/api/v1/users',userRouter)
 app.use('/api/v1/categories',categoryRouter)
 app.use('/api/v1/transactions',transactionRouter)
+
+// Errors
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 8000;
--- middlewares/errorHandlerMiddleware.js
+++ middlewares/errorHandlerMiddleware.js
@@ -2,6 +2,7 @@ const errorHandler = (err, req, res, next) => {
 
     // TODO: Verificar como manejar correctamente el status code.
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode);
     res.json({
         message: err.message,
         stack: err.stack
